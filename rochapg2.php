5c386379816a927a6821d1eb1db01833ad61ded1
furanzu-ro <53499876+furanzu-ro@users.noreply.github.com>
2019-08-12 23:52:27 +0800

    Update rochapg2.php

--- rochapg2.php
+++ rochapg2.php
@@ -8,7 +8,7 @@
 <center>
 <h1>RochaFL Photo Gallery</h1>
 <p><img src="https://vignette.wikia.nocookie.net/fruitsbasket/images/b/ba/Fruits_Basket_2019_Poster_3.jpg/revision/latest?cb=20190317001057" height ="450" width="350"></p>
-<p><a href="rochapg.php">Back To Home</a> 
+<p><a href="index.php">Back To Home</a> 
 <a href="rochapg3.php">Next Photo</a> <p>
 </center>
 
