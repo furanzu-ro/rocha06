d5e2a62da73cb73e6fb10175b630c1c5975e191f
furanzu-ro <53499876+furanzu-ro@users.noreply.github.com>
2019-08-12 23:53:04 +0800

    Update rochapg3.php

--- rochapg3.php
+++ rochapg3.php
@@ -8,7 +8,7 @@
 <center>
 <h1>RochaFL Photo Gallery</h1>
 <p><img src="https://c-lj.gnst.jp/public/article/detail/a/00/03/a0003183/img/basic/a0003183_main.jpg?20190508133015"></p>
-<p><a href="rochapg.php">Back To Home</a> <p>
+<p><a href="index.php">Back To Home</a> <p>
 </center>
 
 
