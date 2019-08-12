3b59f55ebe96f43fe0b5ccc2f637788aedcefa15
furanzu-ro <53499876+furanzu-ro@users.noreply.github.com>
2019-08-12 22:57:04 +0800

    Update and rename rochapg1.html to rochapg1.php

--- rochapg1.php
+++ rochapg1.php
@@ -8,8 +8,8 @@
 <center>
 <h1>RochaFL Photo Gallery</h1>
 <p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRqmjpoHYAlstIjUa8M346vYwJYWusM5fHWV-S3VHpHIWZETd3" height ="150" width="300"></p>
-<p><a href="rochapg.html">Back To Home</a> 
-<a href="rochapg2.html">Next Photo</a> <p>
+<p><a href="rochapg.php">Back To Home</a> 
+<a href="rochapg2.php">Next Photo</a> <p>
 </center>
 
 
