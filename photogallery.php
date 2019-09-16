b2021f6dcf688f65646fd7bd64baadaf9a2226db
furanzu-ro <53499876+furanzu-ro@users.noreply.github.com>
2019-09-16 10:00:23 +0800

    Update photogallery.php

--- photogallery.php
+++ photogallery.php
@@ -3,21 +3,27 @@
 <head>
 <title>Photo Gallery</title>
 </head>
-<body>
+<body background="heyyo.jpg">
 
 <center>
-<h1>RochaFL Photo Gallery</h1>
-<h2>List of Photos</h2>
+<h1 style="font-family:verdana; color:white;">
+RochaFL Photo Gallery
+</h1>
+<h2 style="font-family:courier; color:yellow;">
+List of Photos
+</h2>
+
+
+<section style="font-family:verdana;">
 <p><a href="rochapg1.php">Photo 1</a></p>
 <p><a href="rochapg2.php">Photo 2</a></p>
-<p><a href="rochapg3.php">Photo 3</a></p>
-  
+</p><a href="rochapg3.php">Photo 3</a></p>
+</section>
+ 
 <br>
   <a href= "index.php"><button type="submit">Log-out</button></a>
 </center>
   
-
-
 
 </body>
 </html>
